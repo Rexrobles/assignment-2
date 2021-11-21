2c9be63182ee2a6b814a35623074992798bba937
Rex-Imman Robles <reximman03@gmail.com>
2021-11-21 21:29:15 +0800

    Input from user

---
+++ ASS6/Prog1_numSort.py
@@ -0,0 +1,13 @@
+# Program 1: Number Sorter
+# Create a program that ask 4 numbers. 
+# Print the 4 numbers from highest to lowest using only if-else statement.
+
+# greetings
+print ("Hello! This program can arrange four (4) numbers from highest to lowest.")
+
+# ask inputs from the user
+Num1 = float(input("Enter your first number: "))
+Num2 = float(input("Enter your second number: "))
+Num3 = float(input("Enter your third number: "))
+Num4 = float(input("Enter your fourth number: "))
+
